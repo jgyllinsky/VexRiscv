76a6b510661948ff3f285808addca11bda19277d
Atsushi Yokoyama <yokoyama@flogics.com>
2020-04-06 15:20:08 +0900

    Added PWM functionality to Murax SoC

--- src/main/scala/vexriscv/yokoyama/Murax.scala
+++ src/main/scala/vexriscv/yokoyama/Murax.scala
@@ -2,6 +2,21 @@ package flogics.vexriscv.tinyfpga
 
 import spinal.core._
 import spinal.lib._
+import spinal.lib.bus.amba3.apb._
+import spinal.lib.bus.misc.SizeMapping
+import spinal.lib.bus.simple.PipelinedMemoryBus
+import spinal.lib.com.jtag.Jtag
+import spinal.lib.com.spi.ddr.SpiXdrMaster
+import spinal.lib.com.uart._
+import spinal.lib.io.{InOutWrapper, TriStateArray}
+import spinal.lib.misc.{InterruptCtrl, Prescaler, Timer}
+import spinal.lib.soc.pinsec.{PinsecTimerCtrl, PinsecTimerCtrlExternal}
+import vexriscv.plugin._
+import vexriscv.{VexRiscv, VexRiscvConfig, plugin}
+import spinal.lib.com.spi.ddr._
+import spinal.lib.bus.simple._
+import scala.collection.mutable.ArrayBuffer
+import flogics.vexriscv.pwm._
 import vexriscv.demo._
 
 /**
@@ -24,6 +39,182 @@ import vexriscv.demo._
  * - one UART with tx/rx fifo
  */
 
+
+case class MyMurax(config : MuraxConfig) extends Component{
+  import config._
+
+  val io = new Bundle {
+    //Clocks / reset
+    val asyncReset = in Bool
+    val mainClk = in Bool
+
+    //Main components IO
+    val jtag = slave(Jtag())
+
+    //Peripherals IO
+    val gpioA = master(TriStateArray(gpioWidth bits))
+    val uart = master(Uart())
+    val pwm = out Bool
+
+    val xip = ifGen(genXip)(master(SpiXdrMaster(xipConfig.ctrl.spi)))
+  }
+
+
+  val resetCtrlClockDomain = ClockDomain(
+    clock = io.mainClk,
+    config = ClockDomainConfig(
+      resetKind = BOOT
+    )
+  )
+
+  val resetCtrl = new ClockingArea(resetCtrlClockDomain) {
+    val mainClkResetUnbuffered  = False
+
+    //Implement an counter to keep the reset axiResetOrder high 64 cycles
+    // Also this counter will automatically do a reset when the system boot.
+    val systemClkResetCounter = Reg(UInt(6 bits)) init(0)
+    when(systemClkResetCounter =/= U(systemClkResetCounter.range -> true)){
+      systemClkResetCounter := systemClkResetCounter + 1
+      mainClkResetUnbuffered := True
+    }
+    when(BufferCC(io.asyncReset)){
+      systemClkResetCounter := 0
+    }
+
+    //Create all reset used later in the design
+    val mainClkReset = RegNext(mainClkResetUnbuffered)
+    val systemReset  = RegNext(mainClkResetUnbuffered)
+  }
+
+
+  val systemClockDomain = ClockDomain(
+    clock = io.mainClk,
+    reset = resetCtrl.systemReset,
+    frequency = FixedFrequency(coreFrequency)
+  )
+
+  val debugClockDomain = ClockDomain(
+    clock = io.mainClk,
+    reset = resetCtrl.mainClkReset,
+    frequency = FixedFrequency(coreFrequency)
+  )
+
+  val system = new ClockingArea(systemClockDomain) {
+    val pipelinedMemoryBusConfig = PipelinedMemoryBusConfig(
+      addressWidth = 32,
+      dataWidth = 32
+    )
+
+    //Arbiter of the cpu dBus/iBus to drive the mainBus
+    //Priority to dBus, !! cmd transactions can change on the fly !!
+    val mainBusArbiter = new MuraxMasterArbiter(pipelinedMemoryBusConfig)
+
+    //Instanciate the CPU
+    val cpu = new VexRiscv(
+      config = VexRiscvConfig(
+        plugins = cpuPlugins += new DebugPlugin(debugClockDomain, hardwareBreakpointCount)
+      )
+    )
+
+    //Checkout plugins used to instanciate the CPU to connect them to the SoC
+    val timerInterrupt = False
+    val externalInterrupt = False
+    for(plugin <- cpu.plugins) plugin match{
+      case plugin : IBusSimplePlugin =>
+        mainBusArbiter.io.iBus.cmd <> plugin.iBus.cmd
+        mainBusArbiter.io.iBus.rsp <> plugin.iBus.rsp
+      case plugin : DBusSimplePlugin => {
+        if(!pipelineDBus)
+          mainBusArbiter.io.dBus <> plugin.dBus
+        else {
+          mainBusArbiter.io.dBus.cmd << plugin.dBus.cmd.halfPipe()
+          mainBusArbiter.io.dBus.rsp <> plugin.dBus.rsp
+        }
+      }
+      case plugin : CsrPlugin        => {
+        plugin.externalInterrupt := externalInterrupt
+        plugin.timerInterrupt := timerInterrupt
+      }
+      case plugin : DebugPlugin         => plugin.debugClockDomain{
+        resetCtrl.systemReset setWhen(RegNext(plugin.io.resetOut))
+        io.jtag <> plugin.io.bus.fromJtag()
+      }
+      case _ =>
+    }
+
+
+
+    //****** MainBus slaves ********
+    val mainBusMapping = ArrayBuffer[(PipelinedMemoryBus,SizeMapping)]()
+    val ram = new MuraxPipelinedMemoryBusRam(
+      onChipRamSize = onChipRamSize,
+      onChipRamHexFile = onChipRamHexFile,
+      pipelinedMemoryBusConfig = pipelinedMemoryBusConfig
+    )
+    mainBusMapping += ram.io.bus -> (0x80000000l, onChipRamSize)
+
+    val apbBridge = new PipelinedMemoryBusToApbBridge(
+      apb3Config = Apb3Config(
+        addressWidth = 20,
+        dataWidth = 32
+      ),
+      pipelineBridge = pipelineApbBridge,
+      pipelinedMemoryBusConfig = pipelinedMemoryBusConfig
+    )
+    mainBusMapping += apbBridge.io.pipelinedMemoryBus -> (0xF0000000l, 1 MB)
+
+
+
+    //******** APB peripherals *********
+    val apbMapping = ArrayBuffer[(Apb3, SizeMapping)]()
+    val gpioACtrl = Apb3Gpio(gpioWidth = gpioWidth, withReadSync = true)
+    io.gpioA <> gpioACtrl.io.gpio
+    apbMapping += gpioACtrl.io.apb -> (0x00000, 4 kB)
+
+    val uartCtrl = Apb3UartCtrl(uartCtrlConfig)
+    uartCtrl.io.uart <> io.uart
+    externalInterrupt setWhen(uartCtrl.io.interrupt)
+    apbMapping += uartCtrl.io.apb  -> (0x10000, 4 kB)
+
+    val pwmCtrl = new Apb3PwmCtrl(size = 8)
+    pwmCtrl.io.output <> io.pwm
+    apbMapping += pwmCtrl.io.apb  -> (0x11000, 4 kB)
+
+    val timer = new MuraxApb3Timer()
+    timerInterrupt setWhen(timer.io.interrupt)
+    apbMapping += timer.io.apb     -> (0x20000, 4 kB)
+
+    val xip = ifGen(genXip)(new Area{
+      val ctrl = Apb3SpiXdrMasterCtrl(xipConfig)
+      ctrl.io.spi <> io.xip
+      externalInterrupt setWhen(ctrl.io.interrupt)
+      apbMapping += ctrl.io.apb     -> (0x1F000, 4 kB)
+
+      val accessBus = new PipelinedMemoryBus(PipelinedMemoryBusConfig(24,32))
+      mainBusMapping += accessBus -> (0xE0000000l, 16 MB)
+
+      ctrl.io.xip.fromPipelinedMemoryBus() << accessBus
+      val bootloader = Apb3Rom("src/main/c/murax/xipBootloader/crt.bin")
+      apbMapping += bootloader.io.apb     -> (0x1E000, 4 kB)
+    })
+
+
+
+    //******** Memory mappings *********
+    val apbDecoder = Apb3Decoder(
+      master = apbBridge.io.apb,
+      slaves = apbMapping
+    )
+
+    val mainBusDecoder = new Area {
+      val logic = new MuraxPipelinedMemoryBusDecoder(
+        master = mainBusArbiter.io.masterBus,
+        specification = mainBusMapping,
+        pipelineMaster = pipelineMainBus
+      )
+    }
+  }
+}
 
 object Murax_iCE40_tinyfpga_bx{
 
@@ -65,6 +256,7 @@ object Murax_iCE40_tinyfpga_bx{
       val jtag_tms = in  Bool()
       val uart_txd = out Bool()
       val uart_rxd = in  Bool()
+      val pwm      = out Bool()
       val USBPU    = out Bool()
 
 /*
@@ -76,13 +268,13 @@ object Murax_iCE40_tinyfpga_bx{
 
       val led = out Bits(8 bits)
     }
-    val murax = Murax(
+    val murax = MyMurax(
       MuraxConfig
         .default(withXip = false)
         .copy(
           coreFrequency = 16 MHz,
           onChipRamSize = 8 kB,
-          onChipRamHexFile = "src/main/ressource/hex/muraxDemo.hex"
+          onChipRamHexFile = "src/main/ressource/hex/pwmDemo.hex"
         )
     )
     murax.io.asyncReset := False
@@ -103,6 +295,7 @@ object Murax_iCE40_tinyfpga_bx{
     murax.io.gpioA.read <> 0
     murax.io.uart.txd <> io.uart_txd
     murax.io.uart.rxd <> io.uart_rxd
+    murax.io.pwm <> io.pwm
 
     /*
      * Please refer
---
+++ src/main/scala/vexriscv/yokoyama/PwmCtrlSim.scala
@@ -0,0 +1,86 @@
+package flogics.vexriscv.pwm
+
+import spinal.core._
+import spinal.sim._
+import spinal.core.sim._
+
+package object mine {
+  def my_assert(f: Boolean, msg: String): Unit = {
+    assert(
+      assertion = f,
+      message = msg
+    )
+  }
+}
+
+import mine._
+
+object Apb3PwmCtrlSim {
+  def main(args: Array[String]): Unit = {
+    SimConfig.withWave.doSim(
+      new Apb3PwmCtrl(
+        size = 8
+      )
+    ) { dut =>
+      def wait(count: Int = 1) {
+        dut.clockDomain.waitSampling(count)
+      }
+
+      def write(addr: BigInt, data: BigInt): Unit = {
+        dut.io.apb.PADDR #= addr
+        dut.io.apb.PSEL #= 1
+        dut.io.apb.PENABLE #= false
+        dut.io.apb.PWRITE #= true
+        dut.io.apb.PWDATA #= data
+
+        wait()
+        dut.io.apb.PENABLE #= true
+
+        wait()
+        dut.io.apb.PSEL #= 0
+        dut.io.apb.PENABLE #= false
+      }
+
+      def read_assert(addr: BigInt, expecting: BigInt, msg: String): Unit = {
+        dut.io.apb.PADDR #= addr
+        dut.io.apb.PSEL #= 1
+        dut.io.apb.PENABLE #= false
+        dut.io.apb.PWRITE #= false
+
+        wait()
+        dut.io.apb.PENABLE #= true
+
+        wait()
+        my_assert(
+          dut.io.apb.PRDATA.toBigInt == expecting,
+          msg + " expected PRDATA = " + expecting.toString
+            + ", actual PRDATA = " + dut.io.apb.PRDATA.toBigInt.toString
+        )
+
+        dut.io.apb.PSEL #= 0
+        dut.io.apb.PENABLE #= false
+      }
+
+      dut.clockDomain.forkStimulus(period = 10)
+
+      val REG = 0x0
+
+      /*
+       * Initialize inputs
+       */
+      dut.io.apb.PADDR #= 0
+      dut.io.apb.PSEL #= 0
+      dut.io.apb.PENABLE #= false
+      dut.io.apb.PWRITE #= false
+      dut.io.apb.PWDATA #= 0
+
+      wait(10)
+      write(REG, 0x5a)
+
+      wait(5)
+      read_assert(REG, 0x5a, "reading")
+
+      wait(300)
+    }
+  }
+}
