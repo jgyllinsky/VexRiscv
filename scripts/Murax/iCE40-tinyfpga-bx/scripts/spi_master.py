3af9e905f221394a81d7f4c7548391140a3ccc03
Atsushi Yokoyama <yokoyama@flogics.com>
2020-04-24 15:45:10 +0900

    Cleaned up Python script spi_master.py

--- scripts/Murax/iCE40-tinyfpga-bx/scripts/spi_master.py
+++ scripts/Murax/iCE40-tinyfpga-bx/scripts/spi_master.py
@@ -1,14 +1,13 @@
-# Python3
+#!/usr/bin/env python3
 
-from pyftdi.spi import SpiController # , SpiIOError
+from pyftdi.spi import SpiController
 
 ctrl = SpiController()
 
 ctrl.configure('ftdi://ftdi:232h/1')
-# spi = ctrl.get_port(cs=0, freq=20e6, mode=1)  # maximum
+# spi = ctrl.get_port(cs=0, freq=20e6, mode=1)  # maximum clock test
 spi = ctrl.get_port(cs=0, freq=10e3, mode=1)
 
-# spi.exchange(out=b'1234')
 spi.exchange(out=[0x12, 0x34])
 spi.exchange(out=[0x87, 0x65])
 spi.exchange(out=[0x55, 0xaa])
